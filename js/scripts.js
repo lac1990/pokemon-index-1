8580cd07443ae8221e8b1ea7244256a32dcebb08
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-01-12 22:21:34 -0600

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -4,28 +4,27 @@ let pokemonList = [
         name: 'eevee',
         height: '.3m',
         type: ['normal'],
-        abilities: ['runaway, adaptability, anticipation']
+        abilities: ["runaway", "adaptability", "anticipation"]
     },
     
     {
         name: 'jolteon',
         height: '.8m',
         type: 'electric',
-        abilities: ['volt absorb, quick-feet']
+        abilities: ["volt absorb", "quick-feet"]
     },
 
     {
         name: 'flareon',
         height: '2 11',
         type: 'fire',
-        abilities: ['flash-fire, guts']
+        abilities: ["flash-fire", "guts"]
     },
 
     {
         name: 'vaporeon',
         height: '1m',
         type: 'water',
-        abilities: ['water absorb, hydration']
-    }
-
+        abilities: ["water absorb", "hydration"]
+    },
 ]
