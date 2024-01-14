6be4af4c943b496db28444e9a784a252370677a1
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-01-14 12:42:44 -0600

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,42 @@
+
 let pokemonList = [
     
     {
         name: 'eevee',
-        height: '.3m',
+        height: '.3',
         type: ['normal'],
         abilities: ["runaway", "adaptability", "anticipation"]
     },
     
     {
         name: 'jolteon',
-        height: '.8m',
+        height: '.8',
         type: 'electric',
         abilities: ["volt absorb", "quick-feet"]
     },
 
     {
         name: 'flareon',
-        height: '2 11',
+        height: '.9',
         type: 'fire',
         abilities: ["flash-fire", "guts"]
     },
 
     {
         name: 'vaporeon',
-        height: '1m',
+        height: '1',
         type: 'water',
         abilities: ["water absorb", "hydration"]
     },
-]
+];
+
+for (let i=0; i<pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+    if (pokemonList[i].height <=.9) {
+    document.write('<br>');
+    }
+    else{
+        document.write('That\'s a big pokemon!');
+        document.write('<br>');
+    }
+    }
