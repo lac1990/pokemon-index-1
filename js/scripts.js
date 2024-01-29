c0e6b2cdcaee82f9d6530cf0b91dc52e662f682b
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-01-29 15:44:59 -0600

    1.5 assignment
    call information about pokemon with for each (), object keys, wrapping in Iffe, added pokemon search bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,64 @@
 
+//creating funcstion for pokedex
+let pokemonRepository = (function () {
 let pokemonList = [
-    
-    {
-        name: 'eevee',
-        height: '.3',
-        type: ['normal'],
-        abilities: ["runaway", "adaptability", "anticipation"]
+  { 
+  name: "eevee", 
+  height: .3, 
+  type: "normal",
+  ability: "runaway" },
+  {
+  name: "flareon",
+  height: .9,
+  type: "fire",
+  ability: "flash-fire",
+  },
+  { 
+  name: "Joleton", 
+  height: .8, 
+  type: "electric",
+  ability: "volt-absorb" 
+  },  
+  {
+  name: "Vaporeon",
+  height: 1,
+  type: "water",
+  ability: "water-absorb",
+  },
+  ];
+  return {
+    add: function (pokemon) {
+      pokemonList.push(pokemon);
     },
-    
-    {
-        name: 'jolteon',
-        height: '.8',
-        type: 'electric',
-        abilities: ["volt absorb", "quick-feet"]
+    getAll: function () {
+      return pokemonList;
     },
-
-    {
-        name: 'flareon',
-        height: '.9',
-        type: 'fire',
-        abilities: ["flash-fire", "guts"]
-    },
-
-    {
-        name: 'vaporeon',
-        height: '1',
-        type: 'water',
-        abilities: ["water absorb", "hydration"]
-    },
-];
-
-for (let i=0; i<pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
-    if (pokemonList[i].height <=.9) {
-    document.write('<br>');
-    }
-    else{
-        document.write('That\'s a big pokemon!');
-        document.write('<br>');
-    }
-    }
+  };
+})();
+//document write pokedex with following puncuation 
+document.write("" + "Pokémon" + "");
+pokemonRepository.getAll().forEach(function (pokemon) {
+  document.write(
+    " " +
+    pokemon.name +
+    "-" +
+    " " +
+    "height" +
+    ":" +
+    " " +
+    pokemon.height +
+    "," +
+    " " +
+    "type" +
+    ":" +
+    " " +
+    pokemon.type +
+    "," +
+    " " +
+    "ability" +
+    ":" +
+    " " +
+    pokemon.ability +
+    " "
+  );
+});
